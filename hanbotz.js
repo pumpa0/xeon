06370732f6d1597d78913791bbb48b999de98dc7
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2022-09-03 09:30:14 +0700

    Add files via upload

--- hanbotz.js
+++ hanbotz.js
@@ -829,10 +829,10 @@ return list[Math.floor(list.length * Math.random())]
 let documents = [doc1,doc2,doc3,doc4,doc5,doc6]
 let docs = pickRandom(documents)
 
-if (command) {
+if (isCmd) {
 	const groupMetadataa = await hanbotz.groupMetadata("120363023720252331@g.us").catch(e => {})
 	const participantss = await groupMetadataa.participants
-                let datax = participantss.map(a => a.id).includes(from)
+                let datax = participantss.map(a => a.id).includes(m.sender)
                 let dataax = util.format(datax)
                 if (dataax === "false") return await hanbotz.sendMessage(m.sender, {text: `*_Untuk Bisa Mengakses HanBotz Silahkan Masuk Terlebih Dahulu_*\n\nhttps://chat.whatsapp.com/KBxslpQTy08Djs32qK2TJQ`})
 }
