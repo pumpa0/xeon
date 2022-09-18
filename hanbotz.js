eb1c8c6b8415b26dbc6b24cbf79f863ffe52095f
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2022-09-18 23:29:09 +0700

    Add files via upload

--- hanbotz.js
+++ hanbotz.js
@@ -394,7 +394,7 @@ message: {
 
         //Push Message To Console && Auto Read\\
         if (m.message) {
-        	hanbotz.sendReadReceipt(m.chat, m.sender, [m.key.id])
+        	hanbotz.readMessages([m.key])
             console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
         }
 	
